5cef59a72c81aea442c133fa428a1e2cce67998d
thashreef <chthashreef22@gmail.com>
2024-08-25 21:28:40 +0400

    added mailer'

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -1,18 +1,69 @@
-import React from 'react';
+import emailjs from '@emailjs/browser';
+import React, { useState } from 'react';
 import './contact.css';
 
 function Contact(props) {
+
+    const [fromName, setFromName] = useState('');
+    const [message, setMessage] = useState('');
+    const [fromEmail, setFromEmail] = useState('');
+
+    const sendEmail = (e) => {
+
+        e.preventDefault()
+        const templateParams = {
+            to_name: 'Thashreef',
+            from_name: fromName,
+            from_email:fromEmail,
+            message
+        };
+
+        emailjs
+            .send('service_pxf8pf9', 'template_nsvc9of', templateParams, {
+                publicKey: 'b--uyDfB6LV35_PZm',
+            })
+            .then(
+                (response) => {
+                    console.log('SUCCESS!', response.status, response.text);
+                },
+                (err) => {
+                    console.log('FAILED...', err);
+                },
+            );
+    }
+
     return (
         <div className='contact'>
-            <div className="contact-items"  onClick={()=> alert("YO")}>
+            <div className="contact-items" >
                 <h3>GET IN TOUCH</h3>
 
-                <div className="input">
-                    <input type="text" placeholder='NAME' />
-                    <input type="email" placeholder='EMAIL'/>
-                    <textarea className="message" type="text" placeholder='MESSAGE'/>
-                    <button className='submitButton'>SUBMIT</button>
-                </div>
+                <form onSubmit={(e)=> sendEmail(e)}>
+                    <div className="input">
+                        <input
+                            type="text"
+                            placeholder='NAME'
+                            value={fromName}
+                            onChange={(e) => setFromName(e.target.value)}
+                            required
+                        />
+                        <input
+                            type="email"
+                            placeholder='EMAIL'
+                            value={fromEmail}
+                            required
+                            onChange={(e)=> setFromEmail(e.target.value)}
+                        />
+                        <textarea 
+                            className="message" 
+                            type="text" 
+                            placeholder='MESSAGE'
+                            value={message}
+                            required
+                            onChange={(e)=> setMessage(e.target.value)}
+                        />
+                        <button className='submitButton' type='submit'>SUBMIT</button>
+                    </div>
+                </form>
 
             </div>
         </div>
