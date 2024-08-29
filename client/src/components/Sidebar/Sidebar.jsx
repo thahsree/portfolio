229cac5dd698e032957a90506a20890deb91ac5d
thashreef <chthashreef22@gmail.com>
2024-08-29 23:00:35 +0400

    aminated text size

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 import { useState } from 'react';
 import Links from './Links/Links';
 import './sidebar.css';
@@ -7,33 +7,38 @@ import ToggleButton from './ToggleBar/ToggleButton';
 function Sidebar(props) {
 
     const variants = {
-        open:{
-            clipPath:"circle(1000px at 50px 50px)",
-            transition:{
-                type:'spring',
-                stiffness:20
+        open: {
+            clipPath: "circle(1000px at 50px 50px)",
+            transition: {
+                type: 'spring',
+                stiffness: 20
             }
         },
         closed: {
-            clipPath:"circle(20px at 40px 40px)",
-            transition:{
-                delay:0.1,
-                type:'spring',
-                stiffness:400,
-                damping:40
+            clipPath: "circle(20px at 40px 40px)",
+            transition: {
+                delay: 0.2,
+                type: 'spring',
+                stiffness: 400,
+                damping: 40
             }
         }
     }
 
-    const [isOpen , setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <motion.div className='sidebar' animate={isOpen? 'open' : 'closed'}>
-        
-            <motion.div className="bg" variants={variants}>
-                 <Links setIsOpen={setIsOpen}/>
-            </motion.div>      
-            <ToggleButton setIsOpen={setIsOpen}/>  
+        <motion.div  
+            className='sidebar'
+            animate={isOpen ? 'open' : 'closed'}>
+
+            <motion.div
+                variants={variants}
+                className="bg"
+            >
+                <Links setIsOpen={setIsOpen} />
+            </motion.div>
+            <ToggleButton setIsOpen={setIsOpen} />
         </motion.div>
     );
 }
